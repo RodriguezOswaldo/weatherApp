2cac4621a0033ce121f66d434f16390122b9e4ac
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-10-23 23:37:52 -0600

    city

---
+++ app/src/main/java/com/example/weatherapp/Temperature.java
@@ -0,0 +1,12 @@
+package com.example.weatherapp;
+
+public class Temperature {
+    public String city;
+
+    Temperature(){
+        this.city = city;
+    }
+    void getTemperature() {
+
+    }
+}
