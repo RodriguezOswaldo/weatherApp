bf855c678c2a630be87b60d87aac9ef493bd59aa
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-10-23 23:25:22 -0600

    buttons working

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,16 +2,40 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.cityName) TextView cityName;
+    @BindView(R.id.testText) TextView test;
 
+    @SuppressLint("NonConstantResourceId")
+    @OnClick(R.id.temperature) void getTemperature(View view){
+        Toast.makeText(this,"I'm alive!", Toast.LENGTH_SHORT).show();
+        test.setText((cityName.getText().toString()));
+        System.out.println("It was clicked!");
+    }
+
+    @OnClick(R.id.forecast) void getForecast(View view){
+        test.setText((cityName.getText().toString()));
+        Toast.makeText(this, "I\'m alive as well", Toast.LENGTH_SHORT).show();
+    }
     ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
